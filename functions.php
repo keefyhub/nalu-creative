c779cd9c6ff3eb648e4cb3b0f2164db719f5d0ee
Keith Light <keith.light@strawberrysoup.co.uk>
2018-02-04 20:48:11 +0000

    Remove title support

--- functions.php
+++ functions.php
@@ -11,7 +11,6 @@ if (!function_exists('theme_setup')) {
     function theme_setup()
     {
         add_theme_support('automatic-feed-links');
-        add_theme_support('title-tag');
         add_theme_support('post-thumbnails');
         add_theme_support('html5', [
             'search-form',
